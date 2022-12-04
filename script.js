d827d22bc5a48ebf34947d880bd893961278c4f0
LysieSoyez <Lysie.a.soyez@gmail.com>
2022-12-04 15:30:40 +0100

    mobile finish

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const collection = [
       picture: "assets/Intuitions.jpeg",
       pages: "330 pages",
       resume: "Depuis qu'elle est petite, Jem voit des numéros flotter au-dessus des personnes qu'elle croise. C'est le jour où sa mère décède qu'elle comprend la signification de ce terrible don : ces chiffres indiquent la date de notre mort. Elle se replie alors sur elle-même et sombre dans la mélancolie. Jusqu'au jour où elle rencontre Spider et découvre l'amour. Alors qu'ils partent ensemble à la grande roue de Londres, The London Eye, un phénomène étrange se produit : pourquoi tous les passants ont-ils le même numéro au-dessus de la tête ? Pris de panique, Jem et son ami s'enfuient. Ils seront les seuls survivants de l'attentat, mais aussi les seuls suspects recherchés.",
+      buy:"https://www.fr.fnac.be/a2855061/Intuitions-Tome-01-Intuitions-Rachel-Ward#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Ward-Intuitions-tome-1/179733",
     },
     {
       name: "Les larmes de l'assassin",
@@ -14,6 +16,8 @@ const collection = [
       picture: "assets/Les-larmes-de-l'assassin.jpeg",
       pages: "224 pages",
       resume: "Au sud du Chili, Angel Allegria, un assassin en cavale, a pitié de Paolo Poloverdo, garçon dont il vient de tuer les parents. Il l'élève comme son fils et peu à peu connaît des sentiments nouveaux pour lui : l'amour, la souffrance morale, le remords.",
+      buy:"https://www.fr.fnac.be/a10773883/Anne-Laure-Bondoux-Les-larmes-de-l-assassin#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Bondoux-Les-larmes-de-lassassin/1249235",
     },
     {
       name: "L'affaire Jennifer Jone",
@@ -22,6 +26,8 @@ const collection = [
       picture: "assets/Jennifer-Jones.jpeg",
       pages: "384 pages",
       resume: "Au moment du meurtre, tous les journaux en ont parlé pendant des mois. Des dizaines d'articles ont analysé l'affaire sous tous les angles et les événements de ce jour terrible à Berwick Waters: le contexte, les familles des enfants, les rapports scolaires, les réactions des habitants. La question demeure entière : comment une petite fille de dix ans pouvait-elle tuer un autre enfant ? ",
+      buy:"https://www.fr.fnac.be/a10373973/Nathalie-Laverroux-L-affaire-Jennifer-Jones#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Cassidy-LAffaire-Jennifer-Jones/88493",
     },
     {
       name: "Les enfants rats",
@@ -30,6 +36,8 @@ const collection = [
       picture: "assets/Les-enfants-rats.jpeg",
       pages: "216 pages",
       resume: "Dans un monde politiquement bouleversé, des milliers d'enfants, abandonnés à eux-mêmes, ont trouvé refuge dans les égouts. Regroupés en hordes où règnent la violence et la loi du plus fort, leur quotidien se réduit à voler dans la ville pour survivre, et à échapper à la police ainsi qu'aux autres hordes. Irielle a dix-sept ans. Arrivée dans les égouts à l'âge de dix ans, elle a refusé la loi sauvage des enfants-rats. Elle vit seule avec Jode, un petit garçon de cinq ans qu'elle a trouvé bébé dans une poubelle et à qui elle a appris à lire et à écrire… En ce début de printemps, deux rencontres vont bouleverser leur vie… ",
+      buy:"https://www.fr.fnac.be/a2713926/Francoise-Jay-Les-enfants-rats#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Jay-Les-enfants-rats/156773",
     },
     {
       name: "Le feu secret",
@@ -38,6 +46,8 @@ const collection = [
       picture: "assets/le-feu-secret.jpeg",
       pages: "474 pages",
       resume: "Taylor Montclair est une fille tout ce qu'il y a de plus normal, habitant Woodbury, petite bourgade anglaise endormie. Sacha Winters, jeune homme ténébreux, vit, lui, à Paris, la Ville Lumière. Alors que Taylor ne rêve que d'intégrer la prestigieuse université d'Oxford, Sacha ne s'intéresse nullement aux études. Et pour cause : il sait qu'il va mourir le jour de ses dix-huit ans, dans exactement huit semaines. Et la mort de Sacha verra une antique prophétie se réaliser, entraînant un chaos aux conséquences tragiques pour l'humanité. Taylor est la seule personne qui puisse le sauver. ",
+      buy:"https://www.fr.fnac.be/a8852572/Le-feu-secret-Tome-1-Le-Feu-secret-Christi-Daugherty#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Daugherty-Le-Feu-secret-tome-1/786816",
     },
     {
       name: "Cauchemar d'une nuit d'été",
@@ -46,6 +56,8 @@ const collection = [
       picture: "assets/Cauchemar.jpeg",
       pages: "310 pages",
       resume: "Claire et son amie Délia, qui rêvent de devenir actrice, sont toutes deux embauchées par le père de Claire dont le projet est de tourner le remake d’un film d’horreur des années 1960, resté inachevé après le décès de trois acteurs pendant le tournage. Claire est amoureuse de Jake qui n’a d’yeux que pour Delia follement éprise de Shawn sous le charme de Claire ! Lorsque le tournage commence, il prend très vite une tournure catastrophique : l’actrice qui a repris le rôle de la première comédienne, décédée, se fait trancher le bras dans des conditions similaires, et ce n’est qu’un début…",
+      buy:"https://www.fr.fnac.be/a6079764/R-L-Stine-Cauchemar-d-une-nuit-d-ete#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Stine-Cauchemar-dune-nuit-dete/559562",
     },
     {
       name: "Fahrenheit 451",
@@ -54,6 +66,8 @@ const collection = [
       picture: "assets/fahrenheit.jpeg",
       pages: "236 pages",
       resume: "Montag est un pompier du futur d'un genre particulier : il brûle les livres. Jusqu'au jour où il se met à en lire, refuse le bonheur obligatoire et rêve d'un monde perdu où la littérature et l'imaginaire ne seraient pas bannis. Devenant du coup un dangereux criminel...",
+      buy:"https://www.fr.fnac.be/a1133706/Ray-Bradbury-Fahrenheit-451#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Bradbury-Fahrenheit-451/5315",
     },
     {
       name: "La montagne qui m'a sauvée",
@@ -62,6 +76,8 @@ const collection = [
       picture: "assets/La-Montagne.jpeg",
       pages: "464 pages",
       resume: "Ruinée par la Grande Dépression, la famille d'Ellie quitte la ville pour s'installer au cœur d'une forêt sauvage. Lorsque son père tombe dans le coma à la suite d'un accident dont elle est tenue pour responsable, Ellie part à la recherche d'une sorcière vivant au sommet de la montagne dans l'espoir de le guérir. ",
+      buy:"https://www.fr.fnac.be/a15287520/Lauren-Wolk-La-montagne-qui-m-a-sauvee#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Wolk-La-montagne-qui-ma-sauvee/1305308",
     },
     {
       name: "Frère de sang",
@@ -70,6 +86,8 @@ const collection = [
       picture: "assets/Frères-de-sang.jpeg",
       pages: "144 pages",
       resume: "Dîner tranquille chez les Lemeunier. On sonne à la porte et tout bascule : Brice, le fils aîné, est arrêté par la police, soupçonné d'avoir commis cinq meurtres. Rapidement, les preuves et les mobiles s'accumulent. Pourtant, contre l'avis de tous, Martin reste convaincu de l'innocence de son frère. Armé de cette seule conviction, l'adolescent se lance dans une terrifiante enquête qui va lui révéler d'incroyables secrets. Au péril de sa vie, Martin avance, sans faiblir, prêt à tout pour sauver Brice...",
+      buy:"https://www.fr.fnac.be/a12829326/Mikael-Ollivier-Freres-de-sang#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Massenet-Frere-de-sang/147996",
     },
     {
       name: "Skeleton Creek #1",
@@ -78,6 +96,8 @@ const collection = [
       picture: "assets/skeleton-creek.jpg",
       pages: "187 pages",
       resume: "Il se passe des choses étranges à Skeleton Creek. Des choses terrifiantes. Avec Sarah, ma meilleure amie, j'ai tenté de percer un sinistre secret. J'ai failli en mourir. À présent, je suis coincé chez moi, une jambe dans le plâtre. Je ne peux plus faire confiance à personne. C'est à peine si j'ose croire à ce que j'ai vu et entendu. Depuis que nos parents nous ont interdit de communiquer, Sarah et moi, je suis désespérément seul. Je m'efforce de mettre cette effrayante histoire par écrit pendant que Sarah, armée de sa caméra, poursuit notre enquête. À chacune de ses découvertes, elle m'envoie un mot de passe qui me permet de visionner la vidéo en cachette. Ainsi, ensemble, nous continuons à traquer le mystère, prêts à tout pour exhumer la vérité. Lisez mon journal. Regardez les vidéos. Menez l'enquête.",
+      buy:"https://www.fr.fnac.be/a3402322/Skeleton-Creek-Psychose-Tome-01-Skeleton-Creek-Patrick-Carman#omnsearchpos=1",
+      info:"https://www.babelio.com/livres/Carman-Skeleton-Creek-tome-1--Psychose/1290198",
     }
   ];
   
@@ -94,13 +114,9 @@ const collection = [
 
   //Description of the page
   let para=document.createElement('p');
-  para.textContent="Tu veux lire mais tu ne sais pas quel livre choisir ? Tu veux découvrir de nouveaux livres ? Tu tombes sur cette pages au hasard ? Je te propose de choisir un livre dans la collection si dessous et de te lancer dedans ! Il y a de tout, de l'horreur, du suréaliste, du classique, de la fiction,... Bref ! De quoi trouver ton bonheur."
+  para.textContent="Tu veux lire mais tu ne sais pas quel livre choisir ? Tu veux découvrir de nouveaux livres ? Tu tombes sur cette page au hasard ? Je te propose de choisir un livre dans la collection si dessous et de te lancer dedans ! Il y a de tout, de l'horreur, du suréaliste, du classique, de la fiction,... Bref ! De quoi trouver ton bonheur."
   head_p.appendChild(para);
 
-
-  
-
-  
   
 for(let elem of collection){
 
@@ -161,4 +177,32 @@ for(let elem of collection){
     perso.textContent=elem['resume'];
     perso.className="resume";
     back.appendChild(perso);
+
+    //Div for the icons
+    let div_icons= document.createElement('div');
+    div_icons.className="div_icons";
+    back.appendChild(div_icons);
+
+    //Buy image
+    let buy_link=document.createElement("a");
+    buy_link.setAttribute('target', '_blank');
+    buy_link.href=elem["buy"]
+    div_icons.appendChild(buy_link)
+
+    let buy=document.createElement('img');
+    buy.src="./assets/buy.png";
+    buy_link.appendChild(buy);
+
+    //info image
+
+    let info_link=document.createElement("a");
+    info_link.setAttribute('target', '_blank');
+    info_link.href=elem["info"]
+    div_icons.appendChild(info_link)
+
+    let info= document.createElement('img');
+    info.src="./assets/info.png";
+    info_link.appendChild(info);
+
+  
 }
